d0c26993f768205dc6b035e1fb7676d9573fbf2d
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2022-05-04 15:58:15 +0500

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,10 @@
+echo "+-+-+-+-+-+-+-+-+-+-+ +-+-+-+-+-+-+ +-+-+-+-+-+-+-+
+ |I|n|s|t|a|l|l|i|n|g| |C|o|l|o|u|r| |P|a|c|k|a|g|e|
+ +-+-+-+-+-+-+-+-+-+-+ +-+-+-+-+-+-+ +-+-+-+-+-+-+-+ "
+sleep 1
+cd foreboy 
+make install
+cd
+cd Colour-Package/backboy
+make install 
+cd
