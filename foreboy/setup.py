3384be1f0e48e8b325a9f2e822c18f4e0401f36b
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2022-05-04 15:19:54 +0500

    Create setup.py

---
+++ foreboy/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name = "foreboy",
+    version = "1.0.0",
+    description = "Change text colour",
+    author = "AitzazImtiaz",
+    url = "https://github.com/AitzazImtiaz/Colour-Package",
+    packages = ["soli"],
+    entry_points = {
+        'console_scripts': [
+            'foreboy = foreboy.__main__:main'
+        ]
+    },
+)
